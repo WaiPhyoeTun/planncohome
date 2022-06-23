2d6f8881ecfb4c329602bf7c7d663ff6d2198576
WaiPhyoeTun <waiphyotun567@gmail.com>
2022-06-23 11:20:57 +0630

    23 june 2022

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
 console.log('hi');
 
 $(document).ready(function(){
-	console.log('hey')
+	// Start Header
+	// Start nav
+	// for bugger
+	$('.navbuttons').click(function(){
+		$('.navbuttons').toggleClass("crossxs");
+	});
+
+	// for nav
+	$(window).scroll(function(){
+		let getscrolltop = $(this).scrollTop();
+		// console.log(getscrolltop);
+		if(getscrolltop >= 200){
+			$('.navbar').addClass('navmenus');
+		}else{
+			$(".navbar").removeClass('navmenus')
+		} 
+	});
+	// End nav
+
+
+	// End Header
 })
