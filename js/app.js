56a1a417fe831bace9f2121fdf132bb8f424fd9a
WaiPhyoeTun <waiphyotun567@gmail.com>
2022-07-14 11:45:57 +0630

    git commit--m"14july2022"

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 console.log('hi');
 
 $(document).ready(function(){
+// Start Back to Top 
+
+$(".btn-backtotops").hide();
+    $(window).scroll(function () {
+        let getscrolltop = $(this).scrollTop();
+        console.log(getscrolltop);
+
+        if (getscrolltop >= 370) {
+            $(".btn-backtotops").fadeIn(1000);
+        } else {
+            $(".btn-backtotops").fadeOut(1000);
+        }
+    });
+
+
+// End Back to Top 
+
 	// Start Header
 	// Start nav
 	// for bugger
@@ -22,4 +39,59 @@ $(document).ready(function(){
 
 
 	// End Header
+
+	// Start Properties
+	$(".propertylists").click(function(){
+		// for activeitems
+
+		// $(this).addClass("activeitems");
+		// $(this).siblings().removeClass('activeitems');
+		$(this).addClass("activeitems").siblings().removeClass('.activeitems');
+
+		// for filter
+
+		let getattvalue = $(this).attr('data-filter');
+		// console.log(getattvalue);
+
+		if(getattvalue === "all"){
+			$('.filters').show();
+		}else{
+			$('.filters').hide();
+			$('.filters').not('.'+getattvalue).hide('drop',1500);
+			$('.filters').filter('.'+getattvalue).show('slide',1500)
+		}
+	});
+	// for image overlay or lightbox2
+	lightbox.option({
+      		showImageNumberLabel:false
+    });
+	// End Properties
+
 })
+// Start Adv Section
+$(window).scroll(function(){
+	let getscrolltop = $(this).scrollTop();
+	// console.log(getscrolltop)
+	if(getscrolltop >= 900){
+		$('.advimages').addClass('fromlefts');
+		$('.advtexts').addClass('fromrights');
+	}else{
+		$('.advimages').removeClass('fromlefts');
+		$('.advtexts').removeClass('fromrights')
+	}
+})
+// End Adv Section
+
+// Start Footer
+const getyear = $("#getyear");
+    const getfullyear = new Date().getUTCFullYear();
+    getyear.text(getfullyear);
+
+// End Footer
+
+
+
+// https://www.pexels.com/search/house/
+
+
+// 14PC
